1141e42e2f9d2e616c50e54a0a7908819e8329c9
林裕敏 <bugofbook@gmail.com>
2022-10-28 01:46:57 +0800

    fix: gitflow

--- packages/browser/download-file/release.config.js
+++ packages/browser/download-file/release.config.js
@@ -15,7 +15,7 @@ module.exports = {
         changelogFile: `${srcRoot}/CHANGELOG.md`,
       },
     ],
-    '@semantic-release/npm',
+    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
